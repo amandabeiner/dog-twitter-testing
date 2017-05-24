74a106f2ee2efdeb2d98b4541082645c04fd2c86
Amanda Beiner <aebeiner@gmail.com>
2017-05-24 10:54:55 -0400

    set up beforeEach for stateful components to pass down one data object

--- test/containers/TwitterFeedSpec.js
+++ test/containers/TwitterFeedSpec.js
@@ -1,8 +1,25 @@
 import TwitterFeed from '../../src/containers/TwitterFeed';
+import Tweet from '../../src/components/Tweet';
 
 describe('TwitterFeed', () => {
+  let wrapper;
+  let data = [{
+    id: 1,
+    text: "This doesn't look like Kansas",
+    user: {
+       name: 'Todo',
+       profile_image_url: 'http://images2.fanpop.com/image/quiz/400000/400752_1271413686448_431_300.jpg'
+    }
+  }]
 
   //YOUR TEST CODE HERE
+  beforeEach(() => {
+    wrapper = mount(
+      <TwitterFeed
+        data={data}
+      />
+    )
+  });
 
   
 })
